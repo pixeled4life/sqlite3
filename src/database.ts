f1aa3f305e7122f2e2b02a8d83b405f4ca899255
Eli Ramos <eli.ramos@nsus.dev>
2024-11-07 13:17:03 +0100

    Support typescript's using syntax

--- src/database.ts
+++ src/database.ts
@@ -809,6 +809,10 @@ export class Database {
   [Symbol.for("Deno.customInspect")](): string {
     return `SQLite3.Database { path: ${this.path} }`;
   }
+
+  [Symbol.dispose](): void {
+    this.close();
+  }
 }
 
 const controllers = new WeakMap();
